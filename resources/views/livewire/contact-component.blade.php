75ee3f617b8428e439c46f44f67fee8a4694a57c
Troll-Bonnie <Sahar1student@gmail.com>
2023-09-24 11:03:20 +0200

    contact commit 1

--- resources/views/livewire/contact-component.blade.php
+++ resources/views/livewire/contact-component.blade.php
@@ -4,96 +4,55 @@
 
 <main id="main" class="main-site left-sidebar">
 
+	<section class="contact_section layout_padding">
 		<div class="container">
-
-			<div class="wrap-breadcrumb">
-				<ul>
-					<li class="item-link"><a href="#" class="link">home</a></li>
-					<li class="item-link"><span>Contact us</span></li>
-				</ul>
+		<div class="row">
+			<div class="custom_heading-container ">
+			<h2>
+				Запросити зворотню допомогу
+			</h2>
 			</div>
-			<div class="row">
-				<div class=" main-content-area">
-					<div class="wrap-contacts ">
-						<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-							<div class="contact-box contact-form">
-								<h2 class="box-title">Leave a Message</h2>
-								<form action="#" method="get" name="frm-contact">
+		</div>
+		</div>
+		<div class="container layout_padding2">
+		<div class="row">
+			<div class="col-md-5">
+			<div class="form_contaier">
+				<form>
+				<div class="form-group">
+					<label for="exampleInputName1">Ім'я:</label>
+					<input type="text" class="form-control" id="exampleInputName1">
+				</div>
+				<div class="form-group">
+					<label for="exampleInputNumber1">Телефон:</label>
+					<input type="text" class="form-control" id="exampleInputNumber1">
+				</div>
 
-									<label for="name">Name<span>*</span></label>
-									<input type="text" value="" id="name" name="name" >
-
-									<label for="email">Email<span>*</span></label>
-									<input type="text" value="" id="email" name="email" >
-
-									<label for="phone">Number Phone</label>
-									<input type="text" value="" id="phone" name="phone" >
-
-									<label for="comment">Comment</label>
-									<textarea name="comment" id="comment"></textarea>
-
-									<input type="submit" name="ok" value="Submit" >
-									
-								</form>
-							</div>
-						</div>
-						<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-							<div class="contact-box contact-info">
-								<div class="wrap-map">
-									<div class="mercado-google-maps"
-										 id="az-google-maps57341d9e51968"
-										 data-hue=""
-										 data-lightness="1"
-										 data-map-style="2"
-										 data-saturation="-100"
-										 data-modify-coloring="false"
-										 data-title_maps="Kute themes"
-										 data-phone="088-465 9965 02"
-										 data-email="kutethemes@gmail.com"
-										 data-address="Z115 TP. Thai Nguyen"
-										 data-longitude="-0.120850"
-										 data-latitude="51.508742"
-										 data-pin-icon=""
-										 data-zoom="16"
-										 data-map-type="ROADMAP"
-										 data-map-height="263">
-									</div>
-								</div>
-								<h2 class="box-title">Contact Detail</h2>
-								<div class="wrap-icon-box">
-
-									<div class="icon-box-item">
-										<i class="fa fa-envelope" aria-hidden="true"></i>
-										<div class="right-info">
-											<b>Email</b>
-											<p>Support1@Mercado.com</p>
-										</div>
-									</div>
-
-									<div class="icon-box-item">
-										<i class="fa fa-phone" aria-hidden="true"></i>
-										<div class="right-info">
-											<b>Phone</b>
-											<p>0123-465-789-111</p>
-										</div>
-									</div>
-
-									<div class="icon-box-item">
-										<i class="fa fa-map-marker" aria-hidden="true"></i>
-										<div class="right-info">
-											<b>Mail Office</b>
-											<p>Sed ut perspiciatis unde omnis<br />Street Name, Los Angeles</p>
-										</div>
-									</div>
-
-								</div>
-							</div>
-						</div>
-					</div>
-				</div><!--end main products area-->
-
-			</div><!--end row-->
-
-		</div><!--end container-->
-
+				<div class="form-group">
+					<label for="exampleInputEmail1">Пошта:</label>
+					<input type="email" class="form-control" id="exampleInputEmail1">
+				</div>
+				<div class="form-group">
+					<label for="exampleInputMessage">Повідомлення</label>
+					<input type="text" class="form-control" id="exampleInputMessage">
+				</div>
+				<button type="submit" class="button">Надіслати</button>
+				</form>
+			</div>
+			</div>
+			<div class="col-md-7">
+			<div class="detail-box" href="/shop">
+				<h3>
+				Отримати Медикаменти Зараз
+				</h3>
+				<p style="font-size: 1.1em">
+					Купівля медикаментів в нашому магазині - це швидкий та надійний спосіб забезпечити себе необхідними засобами для здоров'я. 
+					У нас ви знайдете широкий вибір ліків та медичних продуктів високої якості за доступними цінами. 
+					Довірте своє здоров'я професіоналам і зробіть покупку в нашому фармацевтичному магазині вже сьогодні.
+				</p>
+				</a>
+			</div>
+		</div>
+		</div>
+	</section>
 	</main>
